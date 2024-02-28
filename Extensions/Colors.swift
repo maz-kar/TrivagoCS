b52d7a5475cf04266f21d6b3d77ed9161c0265d4
Maziar Layeghkar <maziyarlayeghkar@gmail.com>
2024-02-28 13:56:44 +0100

    background and foreground colors were added.

--- Extensions/Colors.swift
+++ Extensions/Colors.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    static let backgroundColors = BackgroundColors()
+    static let foregroundColors = ForegroundColors()
+}
+
+struct BackgroundColors {
+    let primaryDefaultGreen = Color("PrimaryDefaultGreen")
+    let primaryDefaultBlue = Color("PrimaryDefaultBlue")
+    let primaryPressedGreen = Color("PrimaryPressedGreen")
+    let primaryPressedBlue = Color("PrimaryPressedBlue")
+    let primaryDisabledGray = Color("PrimaryDisabledGray")
+    let secondaryPressedBlueBack = Color("SecondaryPressedBlueBack")
+    let secondaryPressedGreenBack = Color("SecondaryPressedGreenBack")
+    let backgroundBaseButton = Color("BackgroundBaseButton")
+}
+
+struct ForegroundColors {
+    let secondaryDefaultGreen = Color("SecondaryDefaultGreen")
+    let secondaryDefaultBlue = Color("SecondaryDefaultBlue")
+    let secondaryPressedGreenFore = Color("SecondaryPressedGreenFore")
+    let secondaryPressedBlueFore = Color("SecondaryPressedBlueFore")
+    let secondaryDisabledGray = Color("SecondaryDisabledGray")
+    let foregroundBaseButton = Color("ForegroundBaseButton")
+}
