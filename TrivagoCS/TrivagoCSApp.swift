19671be1117fac8d599644136ff2f0c06a328772
Maziar Layeghkar <maziyarlayeghkar@gmail.com>
2024-02-28 13:48:07 +0100

    Initial Commit

---
+++ TrivagoCS/TrivagoCSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivagoCSApp.swift
+//  TrivagoCS
+//
+//  Created by Maziar Layeghkar on 28.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TrivagoCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
